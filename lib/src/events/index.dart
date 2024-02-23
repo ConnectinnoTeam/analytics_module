b674301aacc2fb4a13391be0ca19d68db45d0183
gundogduali <ali.gundogdu@connectinno.com>
2024-02-23 17:10:19 +0300

    export duration event

--- lib/src/events/index.dart
+++ lib/src/events/index.dart
@@ -1,9 +1,10 @@
 export 'abstract/base_event.dart';
 export 'concrete/event/click_event.dart';
+export 'concrete/event/duration_event.dart';
 export 'concrete/event/exit_event.dart';
+export 'concrete/event/purchase_event.dart';
 export 'concrete/event/visit_event.dart';
 export 'concrete/mixin/click_event_mixin.dart';
 export 'concrete/mixin/duration_event_mixin.dart';
 export 'concrete/mixin/exit_event_mixin.dart';
 export 'concrete/mixin/visit_event_mixin.dart';
-export 'concrete/event/purchase_event.dart';
